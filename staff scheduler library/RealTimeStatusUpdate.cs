5041d866e4a4dd4bf3be60b8bbb8ab96c63c6413
diroo-code <khirakashah7@gmail.com>
2024-10-23 22:34:04 +0800

    Add files via upload

---
+++ staff scheduler library/RealTimeStatusUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Google.Cloud.Firestore;
+using Google.Type;
+
+namespace staffschedulerlibrary.Models
+{
+    [FirestoreData]
+    public class RealTimeStatusUpdate
+    {
+        [FirestoreProperty]
+        public int UpdateId { get; set; }
+
+        [FirestoreProperty]
+        public string Message { get; set; }
+
+        [FirestoreProperty]
+        public System.DateTime Timestamp { get; set; }
+        
+
+        public RealTimeStatusUpdate(string message)
+        {
+            Message = message;
+            Timestamp = System.DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Timestamp}] {Message}";
+        }
+    }
+}
---
+++ staff scheduler library/Shift.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using staffschedulerlibrary.Models;
+using Google.Cloud.Firestore;
+
+namespace staffschedulerlibrary.Models
+{
+    [FirestoreData]
+    public class Shift
+    {
+        [FirestoreProperty]
+        public int ShiftId { get; set; }
+
+        [FirestoreProperty]
+        public DateTime StartTime { get; set; }
+
+        [FirestoreProperty]
+        public DateTime EndTime { get; set; }
+
+        [FirestoreProperty]
+        public List<ShiftAssignment> ShiftAssignments { get; set; } // Changed from AssignedStaff
+
+        public Shift()
+        {
+            ShiftAssignments = new List<ShiftAssignment>();
+        }
+
+        public void AddStaff(Staff staff)
+        {
+            var assignment = new ShiftAssignment(staff, this);
+            ShiftAssignments.Add(assignment);
+        }
+    }
+}
